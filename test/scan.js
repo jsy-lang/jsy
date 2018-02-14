66ef25ac2032b7b5790e4f98866c5bb0021861ec
Shane Holloway <shane.holloway@ieee.org>
2018-02-14 12:04:35 -0700

    Expanded test tooling

---
+++ test/scan.js
@@ -0,0 +1,13 @@
+const {promisify} = require('util')
+const readFile = promisify(require('fs').readFile)
+const {jsy_scanner} = require('../dist')
+
+const filename = process.argv.slice(-1)[0]
+readFile(filename, 'utf-8')
+  .then(content =>
+    jsy_scanner(content,
+      {file: `${filename}.js`, source: filename}))
+
+  .then(res => res.src_code())
+  .then(src => new Function(src))
+
