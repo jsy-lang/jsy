e2b45df85964bcd3e2e51d8996d02a4a0d0272f2
Shane Holloway <shane.holloway@ieee.org>
2018-08-09 10:29:13 -0600

    Simplified babel dependency to use babel-plugin-offside-js instead of babel-preset-jsy

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,34 @@
 import * as pkg from './package.json'
-import rpi_jsy from 'rollup-plugin-jsy-babel'
+import rpi_babel from 'rollup-plugin-babel'
+
+const configs = []
+export default configs
 
 const sourcemap = 'inline'
 const external = []
-const plugins = [rpi_jsy()]
+const plugins = [jsy_babel()]
+const plugins_browser = plugins.slice()
 
-export default {
-  input: 'code/index.jsy',
-  output: [
-    { file: pkg.main, sourcemap, format: 'cjs' },
-    { file: pkg.module, sourcemap, format: 'es' },
-  ],
-  plugins, external}
+//import rpi_terser from 'rollup-plugin-terser'
+//plugins_browser.push(rpi_terser())
 
+configs.push(
+  { input: 'code/index.jsy',
+    output: [
+      { file: pkg.main, sourcemap, format: 'cjs' },
+      { file: pkg.module, sourcemap, format: 'es' },
+    ],
+    plugins, external},
+
+  { input: 'code/index.jsy',
+    output: { file: pkg.browser, sourcemap, format: 'umd', name:'jsy_transpile' },
+    plugins: plugins_browser, external})
+
+
+function jsy_babel() {
+  return rpi_babel({
+    presets: [],
+    plugins: ['offside-js'],
+    babelrc: false,
+    exclude: 'node_modules/**',
+  }) }
