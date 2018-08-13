dec5e4533b564254443742c377f971c8be63bcb2
Shane Holloway <shane.holloway@ieee.org>
2018-08-13 08:14:37 -0600

    Added missing files from WalkOffside change

---
+++ test/scanners/test_walk_offside.js
@@ -0,0 +1,155 @@
+require('source-map-support').install()
+const assert = require('assert')
+const {scan_javascript} = require('jsy-transpile/dist/scanner/scan_javascript')
+
+describe @ 'Scanners', @=> ::
+  describe @ 'Walk Offside', @=> ::
+    let offside_ast
+    beforeEach @=> ::
+      // source from https://github.com/shanewholloway/js-consistent-fnvxor32/blob/d2554377c4a540258f93f2958d4259c1f4f03ff9/code/fnvxor32.jsy on 2018-08-09
+      offside_ast = scan_javascript @ `
+        export function hash_fnv32(sz) ::
+          // FNV32, from https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
+          let h = 0x811C9DC5 // fnv-1a 32 bit initial value
+          for let i=0; i < sz.length; i++ ::
+            h ^= sz.charCodeAt(i)
+            h += (h << 24) + (h << 8) + (h << 7) + (h << 4) + (h << 1)
+          return h
+
+        `.replace(/^\s*\r?\n/, '') // trim off the first newline for test stability
+
+
+    it @ 'has debugging support', @=> ::
+      const walk = offside_ast.walk_offside(offside_ast)
+      const lines = walk.toString().split('\n')
+      assert.equal @ lines.length, 9
+
+    it.skip @ 'Can self-verify locations match original source', @=> ::
+
+    it.skip @ 'Can walk a tree of offside indents', @=> ::
+
+    it @ 'matches this offside AST structure', @=> ::
+      check_ast_entry @ offside_ast[0], @{}
+        type: 'offside_line'
+        loc: @{} start: { line: 1, pos: 0, line_pos: 0 }, end: { line: 1, pos: 41, line_pos: 0 }
+        len_indent: 8
+
+        indent: @{}
+           type: 'offside_indent'
+           loc: @{} start: { line: 1, pos: 0, line_pos: 0 }, end: { line: 1, pos: 8, line_pos: 0 }
+           len_indent: 8, indent: '        '
+
+        content: @[]
+         @{} type: 'src'
+             loc: @{} start: { line: 1, pos: 9, line_pos: 0 }, end: { line: 1, pos: 42, line_pos: 0 }
+             content: 'export function hash_fnv32(sz) ::', is_ws: false
+
+
+      check_ast_entry @ offside_ast[1], @{}
+        type: 'offside_line'
+        loc: @{} start: { line: 2, pos: 42, line_pos: 42 }, end: { line: 2, pos: 153, line_pos: 42 }
+        len_indent: 10
+
+        indent: @{}
+           type: 'offside_indent',
+           loc: @{} start: { line: 2, pos: 42, line_pos: 42 }, end: { line: 2, pos: 52, line_pos: 42 }
+           len_indent: 10, indent: '          '
+
+        content: @[]
+         @{} type: 'comment_eol',
+             loc: @{} start: { line: 2, pos: 53, line_pos: 42 }, end: { line: 2, pos: 154, line_pos: 42 }
+             content: '// FNV32, from https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash'
+
+      check_ast_entry @ offside_ast[2], @{}
+        type: 'offside_line'
+        loc: @{} start: { line: 3, pos: 154, line_pos: 154 }, end: { line: 3, pos: 213, line_pos: 154 }
+        len_indent: 10
+
+        indent: @{}
+          type: 'offside_indent',
+          loc: { start: { line: 3, pos: 154, line_pos: 154 }, end: { line: 3, pos: 164, line_pos: 154 } },
+          len_indent: 10, indent: '          '
+
+        content: @[]
+         @{} type: 'src',
+             loc: { start: { line: 3, pos: 165, line_pos: 154 }, end: { line: 3, pos: 184, line_pos: 154 } },
+             content: 'let h = 0x811C9DC5 ', is_ws: false
+
+         @{} type: 'comment_eol',
+             loc: { start: { line: 3, pos: 165, line_pos: 154 }, end: { line: 3, pos: 214, line_pos: 154 } },
+             content: '// fnv-1a 32 bit initial value'
+
+      check_ast_entry @ offside_ast[3], @{}
+        type: 'offside_line',
+        loc: { start: { line: 4, pos: 214, line_pos: 214 }, end: { line: 4, pos: 258, line_pos: 214 } },
+        len_indent: 10
+
+        indent: @{}
+           type: 'offside_indent',
+           loc: { start: { line: 4, pos: 214, line_pos: 214 }, end: { line: 4, pos: 224, line_pos: 214 } },
+           len_indent: 10, indent: '          '
+
+        content: @[]
+         @{} type: 'src',
+             loc: { start: { line: 4, pos: 225, line_pos: 214 }, end: { line: 4, pos: 259, line_pos: 214 } },
+             content: 'for let i=0; i < sz.length; i++ ::', is_ws: false
+
+      check_ast_entry @ offside_ast[4], @{}
+        type: 'offside_line',
+        loc: { start: { line: 5, pos: 259, line_pos: 259 }, end: { line: 5, pos: 292, line_pos: 259 } },
+        len_indent: 12
+
+        indent: @{}
+           type: 'offside_indent'
+           loc: { start: { line: 5, pos: 259, line_pos: 259 }, end: { line: 5, pos: 271, line_pos: 259 } },
+           len_indent: 12, indent: '            '
+
+        content: @[]
+         @{} type: 'src'
+             loc: { start: { line: 5, pos: 272, line_pos: 259 }, end: { line: 5, pos: 293, line_pos: 259 } },
+             content: 'h ^= sz.charCodeAt(i)', is_ws: false
+
+      check_ast_entry @ offside_ast[5], @{}
+        type: 'offside_line'
+        loc: { start: { line: 6, pos: 293, line_pos: 293 }, end: { line: 6, pos: 363, line_pos: 293 } },
+        len_indent: 12
+
+        indent: @{}
+           type: 'offside_indent'
+           loc: { start: { line: 6, pos: 293, line_pos: 293 }, end: { line: 6, pos: 305, line_pos: 293 } },
+           len_indent: 12, indent: '            '
+
+        content: @[]
+         @{} type: 'src'
+             loc: { start: { line: 6, pos: 306, line_pos: 293 }, end: { line: 6, pos: 364, line_pos: 293 } },
+             content: 'h += (h << 24) + (h << 8) + (h << 7) + (h << 4) + (h << 1)', is_ws: false
+
+      check_ast_entry @ offside_ast[6], @{}
+        type: 'offside_line'
+        loc: { start: { line: 7, pos: 364, line_pos: 364 }, end: { line: 7, pos: 382, line_pos: 364 } },
+        len_indent: 10
+
+        indent: @{}
+           type: 'offside_indent'
+           loc: { start: { line: 7, pos: 364, line_pos: 364 }, end: { line: 7, pos: 374, line_pos: 364 } },
+           len_indent: 10, indent: '          '
+
+        content: @[]
+         @{} type: 'src'
+             loc: { start: { line: 7, pos: 375, line_pos: 364 }, end: { line: 7, pos: 383, line_pos: 364 } },
+             content: 'return h', is_ws: false
+
+      check_ast_entry @ offside_ast[7], @{}
+        type: 'offside_blank_line'
+        loc: { start: { line: 8, pos: 383, line_pos: 383 }, end: { line: 8, pos: 383, line_pos: 383 } },
+        is_blank: true
+
+
+function check_ast_entry(ast, structure) ::
+  ast = JSON.parse @ JSON.stringify @ ast
+  try ::
+    assert.deepEqual @ ast, structure
+  catch err ::
+    console.dir @ ast, @{} depth: null
+    throw new Error @ 'AST mismatch'
+
