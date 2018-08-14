ba93a27f785c18a3dcd450ada4c58e6e05a71518
Shane Holloway <shane.holloway@ieee.org>
2018-08-14 09:45:42 -0600

    Added sourcemap support

--- test/scanners/test_scan_jsy.js
+++ test/scanners/test_scan_jsy.js
@@ -1,13 +1,15 @@
 require('source-map-support').install()
+
 const assert = require('assert')
-const {scan_jsy} = require('jsy-transpile/dist/scan_jsy')
-const {transpile_jsy} = require('jsy-transpile/dist/transpile_jsy')
+const { scan_jsy } = require('jsy-transpile/dist/scan_jsy')
+const { transpile_jsy, sourceMapAsComment } = require('jsy-transpile/dist/transpile_jsy')
+const { SourceMapGenerator } = require('source-map')
 
 describe @ 'JSY Scanner', @=> ::
-  let offside_ast
+  let offside_ast, offside_src
   beforeEach @=> ::
     // source from https://github.com/shanewholloway/js-consistent-fnvxor32/blob/d2554377c4a540258f93f2958d4259c1f4f03ff9/code/fnvxor32.jsy on 2018-08-09
-    offside_ast = scan_jsy @ `
+    offside_ast = scan_jsy @ offside_src = `
       const apiUrl = 'http://api.example.com'
 
       class ExampleApi extends SomeBaseClass ::
@@ -55,9 +57,25 @@ describe @ 'JSY Scanner', @=> ::
 
         assert.equal @ part.content, to_source(part)
 
-  it @ 'transpiles', @=> ::
+  it @ 'transpiles to valid JavaScript', @=> ::
     const js_src = transpile_jsy @ offside_ast
     new Function(js_src)
+
+  it @ 'has source-maps', @=> ::
+    const sourcemap = new SourceMapGenerator()
+
+    const source = 'example.jsy'
+    sourcemap.setSourceContent @ source, offside_src
+
+    const js_src = transpile_jsy @ offside_ast, @{}
+      addSourceMapping(arg) ::
+        arg.source = source
+        sourcemap.addMapping(arg)
+      inlineSourceMapping() ::
+        return sourcemap.toString()
+
+    const rx_sourcemap = /^\/\/# sourceMappingURL=/m ;
+    assert.ok @ rx_sourcemap.test(js_src)
 
   it.skip @ 'one-off', @=> ::
     const js_src = transpile_jsy @ `
--- test/transpile.js
+++ test/transpile.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
-const {promisify} = require('util')
-const readFile = promisify(require('fs').readFile)
-const {jsy_scanner} = require('../dist')
+const fs = require('fs').promises
+const {transpile_jsy} = require('../dist/transpile_jsy')
+const {SourceMapGenerator} = require('source-map')
 
-const filename = process.argv.slice(-1)[0]
-readFile(filename, 'utf-8')
-  .then(content =>
-    jsy_scanner(content,
-      {file: `${filename}.js`, source: filename}))
+async function main(filename) {
+  if (!filename) throw new Error('Expected filename')
 
-  .then(res => res.src_code())
-  .then(src => (process.stdout.write(src), src))
-  .then(src => new Function(src))
+  const jsy_src = await fs.readFile(filename, 'utf-8')
 
+  const sourcemap = new SourceMapGenerator()
+  //sourcemap.setSourceContent(filename, jsy_src)
+
+  const src = transpile_jsy(jsy_src, {
+    addSourceMapping(arg) {
+      arg.source = filename
+      sourcemap.addMapping(arg)
+    },
+    inlineSourceMap() {
+      return sourcemap.toString()
+    }
+  })
+
+  process.stdout.write(src)
+}
+
+main(process.argv.slice(2).pop())
