a0135ac5b74a0b6f56f111bd40e01aafe07a5830
Shane Holloway <shane.holloway@ieee.org>
2018-08-29 15:58:09 -0600

    Changed browser unittest to be usable from other websites (URL relative to unittest_root variable)

--- test/browser_unittest.js
+++ test/browser_unittest.js
@@ -4,16 +4,17 @@ const tiny_load = src =>
       Object.assign(
         document.createElement('script'),
         {src, onload, onerror},
-        src.startsWith('http')
+        'string'===typeof src && src.startsWith('http')
           ? {crossorigin: true}
           : null ) ))
 
+const unittest_root = window.unittest_root || window.location
 const deps = [
   'https://unpkg.com/mocha@5.2.0/mocha.js',
   'https://unpkg.com/chai@4.1.2/chai.js',
   'https://unpkg.com/acorn@5.7.2/dist/acorn.js',
   'https://unpkg.com/source-map@0.7.3/dist/source-map.js',
-  './umd/jsy-transpile.js',
+  new URL('./umd/jsy-transpile.js', unittest_root),
 ]
 
 Promise
@@ -23,7 +24,7 @@ Promise
   window['source-map'] = window.sourceMap
   window.require = module => window[module]
 
-  await tiny_load('./test/__unittest.iife.js')
+  await tiny_load(new URL('./test/__unittest.iife.js', unittest_root))
 
   mocha.run()
 })
