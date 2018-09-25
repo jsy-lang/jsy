91b9b920b49916c239326d35966508f861bf705c
Shane Holloway <shane.holloway@ieee.org>
2018-09-25 16:28:31 -0600

    Changed transpile bin script to accept stdin input;  allow transpiling when the source-map package is not available

--- transpile.js
+++ transpile.js
@@ -1,27 +1,49 @@
 #!/usr/bin/env node
-const fs = require('fs').promises
+const fs = require('fs')
 const { jsy_transpile } = require('jsy-transpile')
-const { SourceMapGenerator } = require('source-map')
+
+let SourceMapGenerator
+try { SourceMapGenerator = require('source-map').SourceMapGenerator } catch (err) {}
 
 async function main(filename) {
-  if (!filename) throw new Error('Expected filename')
+  const jsy_src = filename
+    ? await fs.promises.readFile(filename, 'utf-8')
+    : await read_stdin()
 
-  const jsy_src = await fs.readFile(filename, 'utf-8')
-
-  const sourcemap = new SourceMapGenerator()
+  const sourcemap = filename && SourceMapGenerator && new SourceMapGenerator()
   //sourcemap.setSourceContent(filename, jsy_src)
 
   const src = jsy_transpile(jsy_src, {
     addSourceMapping(arg) {
+      if (!sourcemap) return
       arg.source = filename
       sourcemap.addMapping(arg)
     },
     inlineSourceMap() {
+      if (!sourcemap) return
       return sourcemap.toString()
     }
   })
 
   process.stdout.write(src)
 }
+
+function read_stdin(stdin) {
+  // from MIT © Sindre Sorhus, https://github.com/sindresorhus/get-stdin/blob/83c5c9f9059f0a66a6c9acef03c19546e2cb175b/index.js
+  if (null == stdin)
+    stdin = process.stdin
+  return new Promise(resolve => {
+		if (stdin.isTTY)
+			return resolve(null)
+
+		stdin.setEncoding('utf8')
+
+    let ret = ''
+		stdin.on('end', () => resolve(ret))
+		stdin.on('readable', () => {
+      let chunk
+			while (chunk = stdin.read())
+				ret += chunk
+		}) }) }
 
 main(process.argv.slice(2).pop())
