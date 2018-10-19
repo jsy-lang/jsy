3ae4679a76226b9e8ee551def092b74369d6eb02
Shane Holloway <shane.holloway@ieee.org>
2018-10-19 13:09:33 -0600

    Added **alpha** syntax for immediately invoked function expressions, including async and generator expressions.

---
+++ test/unittest/testIIFE.js
@@ -0,0 +1,166 @@
+import { genMochaSyntaxTestCases, standardTransforms } from './_xform_syntax_variations'
+
+describe @ 'Immediately Invoked Function Expressions',
+  genMochaSyntaxTestCases @ iterImmediatelyInvokedFunctionExpressionTests, standardTransforms
+
+describe @ 'Immediately Invoked Async Function Expressions',
+  genMochaSyntaxTestCases @ iterImmediatelyInvokedGeneratorExpressionTests, standardTransforms
+
+describe @ 'Immediately Invoked Function Expressions (Variants)',
+  genMochaSyntaxTestCases @ iterImmediatelyInvokedGeneratorExpressionVariationsTests
+
+
+function * iterImmediatelyInvokedFunctionExpressionTests() ::
+  yield * iterIIArrowExpressions()
+  yield * iterIIFuncExpressions()
+
+  yield * iterIIAsyncArrowExpressions()
+  yield * iterIIAsyncFuncExpressions()
+
+function * iterImmediatelyInvokedGeneratorExpressionTests() ::
+  yield * iterIIGeneratorExpressions('@!*')
+  yield * iterIIAsyncGeneratorExpressions('@!>*')
+
+function * iterImmediatelyInvokedGeneratorExpressionVariationsTests() ::
+  yield * iterIIGeneratorExpressions('@!=>*')
+
+  yield * iterIIAsyncGeneratorExpressions('@!*>')
+  yield * iterIIAsyncGeneratorExpressions('@!=>>*')
+
+
+function * iterIIArrowExpressions() ::
+  yield @{}
+    title: 'arrow expression (@!=>)'
+    source: '@!=> expression_body'
+    tokens: @[] '(', '(', ')', '=>', 'name', ')', '(', ')'
+
+  yield @{}
+    title: 'arrow expression - two lines (@!=>)'
+    source: @[]
+      '@!=>'
+      '  expression_body'
+    tokens: @[] '(', '(', ')', '=>', 'name', ')', '(', ')'
+
+  yield @{}
+    title: 'arrow block expression (@!=>)'
+    source: @[]
+      '@!=> ::'
+      '  stmt_a'
+      '  stmt_b'
+    tokens: @[] '(', '(', ')', '=>', '{', 'name', 'name', '}', ')', '(', ')'
+
+
+function * iterIIAsyncArrowExpressions() ::
+  yield @{}
+    title: 'async arrow expression (@!=>>)'
+    source: '@!=>> await expression_body'
+    tokens: @[] '(', 'name', '(', ')', '=>', 'name', 'name', ')', '(', ')'
+
+  yield @{}
+    title: 'async arrow expression - two lines (@!=>>)'
+    source: @[]
+      '@!=>>'
+      '  await expression_body'
+    tokens: @[] '(', 'name', '(', ')', '=>', 'name', 'name', ')', '(', ')'
+
+  yield @{}
+    title: 'async arrow block expression (@!=>>)'
+    source: @[]
+      '@!=>> ::'
+      '  await stmt_a'
+      '  await stmt_b'
+    tokens: @[] '(', 'name', '(', ')', '=>', '{', 'name', 'name', 'name', 'name', '}', ')', '(', ')'
+
+
+function * iterIIFuncExpressions() ::
+  yield @{}
+    title: 'immediately invoked expression (@!)'
+    source: '@! expression_body'
+    tokens: @[] '(', '(', ')', '=>', '{', 'name', '}', ')', '(', ')'
+
+  yield @{}
+    title: 'immediately invoked expression - two lines (@!)'
+    source: @[]
+      '@!'
+      '  stmt_a'
+      '  stmt_b'
+    tokens: @[] '(', '(', ')', '=>', '{', 'name', 'name', '}', ')', '(', ')'
+
+  yield @{}
+    title: 'immediately invoked expression (@!)'
+    source: @[]
+      '@! ::'
+      '  stmt_a'
+      '  stmt_b'
+    tokens: @[] '(', '(', ')', '=>', '{', '{', 'name', 'name', '}', '}', ')', '(', ')'
+
+
+function * iterIIAsyncFuncExpressions() ::
+  yield @{}
+    title: 'async immediately invoked expression (@!>)'
+    source: '@!> await expression_body'
+    tokens: @[] '(', 'name', '(', ')', '=>', '{', 'name', 'name', '}', ')', '(', ')'
+
+  yield @{}
+    title: 'async immediately invoked expression - two lines (@!>)'
+    source: @[]
+      '@!>'
+      '  await stmt_a'
+      '  await stmt_b'
+    tokens: @[] '(', 'name', '(', ')', '=>', '{', 'name', 'name', 'name', 'name', '}', ')', '(', ')'
+
+  yield @{}
+    title: 'async immediately invoked expression (@!>)'
+    source: @[]
+      '@!> ::'
+      '  await stmt_a'
+      '  await stmt_b'
+    tokens: @[] '(', 'name', '(', ')', '=>', '{', '{', 'name', 'name', 'name', 'name', '}', '}', ')', '(', ')'
+
+
+function * iterIIGeneratorExpressions(variant='@!*') ::
+  yield @{}
+    title: `immediately invoked generator expression (${variant})`
+    source: `${variant} yield expression_body`
+    tokens: @[] '(', 'function', '*', '(', ')', '{', 'name', 'name', '}', ')', '.', 'name', '(', 'this', ')'
+
+  yield @{}
+    title: `immediately invoked generator expression - two lines (${variant})`
+    source: @[]
+      variant
+      '  yield stmt_a'
+      '  yield stmt_b'
+    tokens: @[] '(', 'function', '*', '(', ')', '{', 'name', 'name', 'name', 'name', '}', ')', '.', 'name', '(', 'this', ')'
+
+  yield @{}
+    title: `immediately invoked generator expression (${variant})`
+    source: @[]
+      `${variant} ::`
+      '  yield stmt_a'
+      '  yield stmt_b'
+    tokens: @[] '(', 'function', '*', '(', ')', '{', '{', 'name', 'name', 'name', 'name', '}', '}', ')', '.', 'name', '(', 'this', ')'
+
+
+
+function * iterIIAsyncGeneratorExpressions(variant='@!>*') ::
+  yield @{}
+    title: `async immediately invoked generator expression (${variant})`
+    source: `${variant} yield await expression_body`
+    tokens: @[] '(', 'name', 'function', '*', '(', ')', '{', 'name', 'name', 'name', '}', ')', '.', 'name', '(', 'this', ')'
+
+  yield @{}
+    title: `async immediately invoked generator expression - two lines (${variant})`
+    source: @[]
+      variant
+      '  yield await stmt_a'
+      '  yield await stmt_b'
+    tokens: @[] '(', 'name', 'function', '*', '(', ')', '{', 'name', 'name', 'name', 'name', 'name', 'name', '}', ')', '.', 'name', '(', 'this', ')'
+
+  yield @{}
+    title: `async immediately invoked generator expression (${variant})`
+    source: @[]
+      `${variant} ::`
+      '  yield await stmt_a'
+      '  yield await stmt_b'
+    tokens: @[] '(', 'name', 'function', '*', '(', ')', '{', '{', 'name', 'name', 'name', 'name', 'name', 'name', '}', '}', ')', '.', 'name', '(', 'this', ')'
+
