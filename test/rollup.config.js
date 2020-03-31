ab83533e103d0ac63c971b25e2453ce7cc823e7d
Shane Holloway <shane.holloway@ieee.org>
2020-03-30 22:33:24 -0600

    Fixed unittest using missing rpi_jsy.mjs

--- test/rollup.config.js
+++ test/rollup.config.js
@@ -1,25 +1,19 @@
 import rpi_resolve from '@rollup/plugin-node-resolve'
 import rpi_commonjs from '@rollup/plugin-commonjs'
-import rpi_bound_jsy_lite from '../rpi_jsy.mjs'
-
+import rpi_jsy from '../stable/rollup-jsy-bootstrap.mjs'
 
 const sourcemap = 'inline'
-const external = []
-
-const rpi_jsy = rpi_bound_jsy_lite()
-const plugins = [rpi_jsy]
-
-// unittesting compile rollup
-const test_plugins = plugins.concat([
+const plugins = [
+  rpi_jsy(),
   rpi_resolve(),
   rpi_commonjs({ include: 'node_modules/**'}),
-])
+]
 
 export default [
-  { input: `./unittest.jsy`, context: 'window', plugins: test_plugins,
+  { input: `./unittest.jsy`, context: 'window', plugins,
     output: { file: './__unittest.iife.js', format: 'iife', name: `test_jsy_transpile`, sourcemap } },
 
-  { input: `./unittest.jsy`, plugins: test_plugins,
+  { input: `./unittest.jsy`, plugins,
     output: { file: './__unittest.cjs.js', format: 'cjs', sourcemap } },
 
 ].filter(Boolean)
